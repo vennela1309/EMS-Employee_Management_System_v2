79f7a40ed1b1adad56d5c92557f9da2bf60d556c
vennela1309 <vennelakristhu@gmail.com>
2026-06-23 12:48:24 +0530

    db connection setup

--- EMSBackend/server.js
+++ EMSBackend/server.js
@@ -1,18 +1,30 @@
-const express = require("express")
-const cors = require("cors")
-require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const db = require("./config/db");
 
 const app = express();
 
-//middleware 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("EMS Backend running Successfully")
-})
+db.getConnection()
+  .then((connection) => {
+    console.log("MySQL Connected Successfully");
+    connection.release();
+  })
+  .catch((err) => {
+    console.error("Database Connection Failed");
+    console.error(err.message);
+  });
+
+app.get("/", (req, res) => {
+  res.send("EMS Backend Running Successfully");
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT , ()=>{
-    console.log("Server is running on 5000")
-})
+
+app.listen(PORT, () => {
+  console.log(` Server is running on port ${PORT}`);
+});
